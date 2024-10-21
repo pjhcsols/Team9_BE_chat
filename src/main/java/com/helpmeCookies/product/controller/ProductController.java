a0da26fd42f09cd6bcaf62eace4b92f5ebc35b45
bokyeong <wntjdals0412@gmail.com>
2024-10-22 02:46:42 +0900

    refactor: [#61] 사진 수정 로직 변경 사진 수정시 s3에 먼저 업로드 후 데이터베이스에 저장

--- src/main/java/com/helpmeCookies/product/controller/ProductController.java
+++ src/main/java/com/helpmeCookies/product/controller/ProductController.java
@@ -1,6 +1,6 @@
 package com.helpmeCookies.product.controller;
 
-import com.helpmeCookies.product.dto.FileUploadResponse;
+import com.helpmeCookies.product.dto.ImageUpload;
 import com.helpmeCookies.product.dto.ProductImageResponse;
 import com.helpmeCookies.product.dto.ProductRequest;
 import com.helpmeCookies.product.dto.ProductResponse;
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -25,14 +24,15 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<Void> saveProduct(@RequestBody ProductRequest productRequest) {
-        productService.save(productRequest);
+        Product product = productService.save(productRequest);
+        productImageService.saveImages(product.getId(),productRequest.productImages());
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/{productId}/images")
-    public ResponseEntity<ProductImageResponse> uploadImages(@PathVariable("productId") Long productId, List<MultipartFile> files) {
-        List<FileUploadResponse> responses = productImageService.uploadMultiFiles(productId,files);
-        return ResponseEntity.ok(new ProductImageResponse(responses.stream().map(FileUploadResponse::photoUrl).toList()));
+    @PostMapping("/images")
+    public ResponseEntity<ProductImageResponse> uploadImages(List<MultipartFile> files) {
+        List<ImageUpload> responses = productImageService.uploadMultiFiles(files);
+        return ResponseEntity.ok(new ProductImageResponse(responses.stream().map(ImageUpload::photoUrl).toList()));
     }
 
     @GetMapping("/{productId}")
@@ -50,8 +50,10 @@ public class ProductController {
     }
 
     @PutMapping("/{productId}/images")
-    public ResponseEntity<Void> editImages(@PathVariable("productId") Long productId, List<MultipartFile> files) throws IOException {
+    public ResponseEntity<Void> editImages(@PathVariable("productId") Long productId, List<MultipartFile> files) {
         productImageService.editImages(productId, files);
+        List<ImageUpload> images = productImageService.uploadMultiFiles(files);
+        productImageService.saveImages(productId,images);
         return ResponseEntity.ok().build();
     }
 
--- src/main/java/com/helpmeCookies/product/service/ProductImageService.java
+++ src/main/java/com/helpmeCookies/product/service/ProductImageService.java
@@ -2,7 +2,6 @@ package com.helpmeCookies.product.service;
 
 import com.helpmeCookies.global.utils.AwsS3FileUtils;
 import com.helpmeCookies.product.dto.ImageUpload;
-import com.helpmeCookies.product.dto.ProductRequest;
 import com.helpmeCookies.product.entity.ProductImage;
 import com.helpmeCookies.product.repository.ProductImageRepository;
 import java.util.ArrayList;
@@ -20,7 +19,7 @@ public class ProductImageService {
     private final ProductImageRepository productImageRepository;
 
     @Transactional
-    public List<ImageUpload> uploadMultiFiles(Long productId, List<MultipartFile> files) {
+    public List<ImageUpload> uploadMultiFiles(List<MultipartFile> files) {
         List<ImageUpload> imageUploads = new ArrayList<>();
         for (MultipartFile multipartFile:files) {
             imageUploads.add(awsS3FileUtils.uploadMultiImages(multipartFile));
@@ -29,16 +28,16 @@ public class ProductImageService {
     }
 
     @Transactional
-    public void saveImages(Long productId,ProductRequest productRequest) {
-        productRequest.productImages().forEach(image -> productImageRepository.save(image.toEntity(productId)));
+    public void saveImages(Long productId,List<ImageUpload> files) {
+        files.forEach(image -> productImageRepository.save(image.toEntity(productId)));
     }
 
     @Transactional
     public void editImages(Long productId, List<MultipartFile> files) {
         //우선은 전부 삭제하고 다시 업로드
         //추후에 개선 예정
+        //TODO s3서버에서 기존 사진들을 제거하는 기능
         productImageRepository.deleteAllByProductId(productId);
-        uploadMultiFiles(productId, files);
     }
 
     public List<String> getImages(Long productId) {
