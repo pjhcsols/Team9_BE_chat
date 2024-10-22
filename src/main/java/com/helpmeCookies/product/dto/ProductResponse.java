dcd12d180771b67d661da21c461144886161c0ba
bokyeong <wntjdals0412@gmail.com>
2024-10-22 15:34:13 +0900

    feat : [#45] 상품 상세 조회시 관련 이미지 url 반환

--- src/main/java/com/helpmeCookies/product/dto/ProductResponse.java
+++ src/main/java/com/helpmeCookies/product/dto/ProductResponse.java
@@ -14,12 +14,13 @@ public record ProductResponse(
         String description,
         String preferredLocation,
         List<HashTag> hashTags,
-        ArtistInfo artistInfo
+        ArtistInfo artistInfo,
+        List<String> imageUrls
 ) {
     public record ArtistInfo(Long artistId, String artistName) {
     }
 
-    public static ProductResponse from(Product product) {
+    public static ProductResponse from(Product product, List<String> urls) {
         return new ProductResponse(
                 product.getId(),
                 product.getName(),
@@ -29,7 +30,8 @@ public record ProductResponse(
                 product.getDescription(),
                 product.getPreferredLocation(),
                 product.getHashTags(),
-                new ArtistInfo(product.getArtistInfo().getId(),product.getArtistInfo().getNickname())
+                new ArtistInfo(product.getArtistInfo().getId(),product.getArtistInfo().getNickname()),
+                urls
         );
     }
 }
