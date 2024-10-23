3e0fc6b7f08c4f3c3a4596f773d77234b3c9bbdf
bokyeong <wntjdals0412@gmail.com>
2024-10-24 01:27:38 +0900

    feat : [#62] 감상평 작성 기능 사용자가 작품을 구매하였는지 확인X

---
+++ src/main/java/com/helpmeCookies/review/controller/ReviewController.java
@@ -0,0 +1,30 @@
+package com.helpmeCookies.review.controller;
+
+import com.helpmeCookies.review.dto.ReviewRequest;
+import com.helpmeCookies.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    //TODO 감상평 등록
+    //TODO 감상평 수정
+    //TODO 감상평 삭제
+    //TODO 해당 상품의 감상평 조회
+    //TODO 내 감상평 조회
+
+    @PostMapping("/{productId}")
+    public ResponseEntity<Void> postReview(@RequestBody ReviewRequest request, Long productId) {
+        reviewService.saveReview(request,productId);
+        return ResponseEntity.ok().build();
+    }
+}
