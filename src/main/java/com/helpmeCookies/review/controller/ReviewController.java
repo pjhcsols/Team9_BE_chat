2b89c6b05a37d14a566d650b95432f9c50665b20
bokyeong <wntjdals0412@gmail.com>
2024-10-24 17:20:59 +0900

    fix : [#62] 감상평 등록 오류 수정

--- src/main/java/com/helpmeCookies/review/controller/ReviewController.java
+++ src/main/java/com/helpmeCookies/review/controller/ReviewController.java
@@ -4,6 +4,7 @@ import com.helpmeCookies.review.dto.ReviewRequest;
 import com.helpmeCookies.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +17,13 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    //TODO 감상평 등록
     //TODO 감상평 수정
     //TODO 감상평 삭제
     //TODO 해당 상품의 감상평 조회
     //TODO 내 감상평 조회
 
     @PostMapping("/{productId}")
-    public ResponseEntity<Void> postReview(@RequestBody ReviewRequest request, Long productId) {
+    public ResponseEntity<Void> postReview(@RequestBody ReviewRequest request, @PathVariable Long productId) {
         reviewService.saveReview(request,productId);
         return ResponseEntity.ok().build();
     }
