880f720778db3e9f6649d097c4a15ec09510af36
bokyeong <wntjdals0412@gmail.com>
2024-11-08 15:16:19 +0900

    feat : [#62] 한 상품에 대한 리뷰 조회 기능 Page 형식으로 반환

--- src/main/java/com/helpmeCookies/review/service/ReviewService.java
+++ src/main/java/com/helpmeCookies/review/service/ReviewService.java
@@ -3,12 +3,15 @@ package com.helpmeCookies.review.service;
 import com.helpmeCookies.product.entity.Product;
 import com.helpmeCookies.product.repository.ProductRepository;
 import com.helpmeCookies.review.dto.ReviewRequest;
+import com.helpmeCookies.review.dto.ReviewResponse;
 import com.helpmeCookies.review.entity.Review;
 import com.helpmeCookies.review.repository.ReviewRepository;
 import com.helpmeCookies.user.entity.User;
 import com.helpmeCookies.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class ReviewService {
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
 
+    @Transactional
     public void saveReview(ReviewRequest request, Long productId) {
         User writer = userRepository.findById(request.writerId()).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 writerID입니다."));
         Product product = productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 productID입니다."));
@@ -24,12 +28,25 @@ public class ReviewService {
         reviewRepository.save(request.toEntity(writer,product));
     }
 
+    @Transactional
     public void editReview(ReviewRequest request, Long productId, Long reviewId) {
         Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 reviewId 입니다."));
         review.updateContent(request.content());
     }
 
+    @Transactional(readOnly = true)
     public Review getReview(Long reviewId) {
         return reviewRepository.findById(reviewId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 reviewId 입니다."));
+    }
+
+    @Transactional
+    public void deleteReview(Long reviewId) {
+        reviewRepository.deleteById(reviewId);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ReviewResponse> getAllReview(Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 productId입니다." + productId));
+        return reviewRepository.findAllByProduct(product).map(ReviewResponse::fromEntity);
     }
  }
