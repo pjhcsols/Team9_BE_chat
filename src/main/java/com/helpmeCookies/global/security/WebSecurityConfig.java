7593f65c23b054c885714926e6622eaeb6f04b9d
yooonwodyd <dbswodyd00@naver.com>
2024-11-15 19:59:51 +0900

    refactor:[#84]- refact Security
    cors 변경

--- src/main/java/com/helpmeCookies/global/security/WebSecurityConfig.java
+++ src/main/java/com/helpmeCookies/global/security/WebSecurityConfig.java
@@ -40,6 +40,7 @@ public class WebSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+		http.cors((cors) -> cors.configurationSource(corsConfigurationSource()));
 		http
 			.securityMatcher("/oauth2/**")
 			.csrf(AbstractHttpConfigurer::disable)
@@ -101,6 +102,7 @@ public class WebSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
+		http://1.618.s3-website.ap-northeast-2.amazonaws.com'
 		configuration.setAllowedOrigins(Arrays.asList("http://1.618.s3-website.ap-northeast-2.amazonaws.com","http://localhost:3000"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
