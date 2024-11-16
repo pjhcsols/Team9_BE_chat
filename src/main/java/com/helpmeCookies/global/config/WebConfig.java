1e6888a2520e0cfd76979ffbb5b1dd456d6035e0
donghyuun <bomul0524@gmail.com>
2024-11-16 13:24:14 +0900

    test: cors error resolve test 5

--- src/main/java/com/helpmeCookies/global/config/WebConfig.java
+++ src/main/java/com/helpmeCookies/global/config/WebConfig.java
@@ -6,21 +6,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/**")
-//			.allowedOrigins("http://1.618.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000")
-//			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//			.allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers","cross-control-allow-origin")
-//			.allowCredentials(true);
-//	}
-
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOriginPatterns("http://1.618.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000") // 허용할 도메인 (모든 도메인 허용: "*")
-			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-			.allowedHeaders("*") // 허용할 헤더
-			.allowCredentials(true); // 인증 정보 허용 여부
+			.allowedOrigins("http://1.618.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers","cross-control-allow-origin")
+			.allowCredentials(true);
 	}
+
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//			.allowedOriginPatterns("http://1.618.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000") // 허용할 도메인 (모든 도메인 허용: "*")
+//			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+//			.allowedHeaders("*") // 허용할 헤더
+//			.allowCredentials(true); // 인증 정보 허용 여부
+//	}
 }
